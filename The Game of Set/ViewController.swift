3067bdd01eca1a837cc1e514400d79054ed180ea
Cassidy Caid <cassidycaid@gmail.com>
2019-02-06 20:43:35 -0500

    Initial Commit

---
+++ The Game of Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Game of Set
+//
+//  Created by Cassidy Caid on 2/6/19.
+//  Copyright © 2019 Cassidy Caid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
