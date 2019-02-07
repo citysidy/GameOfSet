329de09e97c4bc095065d44960bab174c55776b8
Cassidy Caid <cassidycaid@gmail.com>
2019-02-06 23:35:16 -0500

    Add Version 1 of SetCard struct

--- The Game of Set/Model/SetCard.swift
+++ The Game of Set/Model/SetCard.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+
+struct SetCard: Hashable, CustomStringConvertible {
+    
+    var hashValue : Int {return cardHash}
+    var description: String {return "\(color)\(shape)\(fill)\(count)"}
+    
+    static func == (lhs: SetCard, rhs: SetCard) -> Bool {
+        return lhs.cardHash == rhs.cardHash
+    }
+    
+    private var cardHash: Int {
+        return Int(color.description + shape.description + fill.description + count.description) ?? 10000
+    }
+    
+    var color: Color
+    var shape: Shape
+    var fill: Fill
+    var count: Count
+    
+    enum Color: String, CustomStringConvertible {
+        var description: String {return String(rawValue.last ?? "0")}
+        
+        case color1
+        case color2
+        case color3
+        
+        static var all = [Color.color1, .color2, .color3]
+    }
+    
+    enum Shape: String, CustomStringConvertible {
+        var description: String {return String(rawValue.last ?? "0")}
+        
+        case shape1
+        case shape2
+        case shape3
+        
+        static var all = [Shape.shape1, .shape2, .shape3]
+    }
+    
+    enum Fill: String, CustomStringConvertible {
+        var description: String {return String(rawValue.last ?? "0")}
+        
+        case fill1
+        case fill2
+        case fill3
+        
+        static var all = [Fill.fill1, .fill2, .fill3]
+    }
+    
+    enum Count: String, CustomStringConvertible {
+        var description: String {return String(rawValue.last ?? "0")}
+        
+        case count1
+        case count2
+        case count3
+        
+        static var all = [Count.count1, .count2, .count3]
+    }
+}
+
